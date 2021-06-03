3702c8758d9c9575e7cd4fb0b5b6c7ce76378d10
Marc Buma <mosbuma@bumos.nl>
2021-06-03 13:14:52 +0200

    disable wait for nodes, connect to node A

--- multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
+++ multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 # wait-for-nodes.sh
 set -e
-WAIT_S=180
-  
->&2 echo "DRX backend server - Waiting $WAIT_S seconds to allow nodes to start up"
-sleep $WAIT_S
+# wait is no longer necessary - backend polls connection now
+# WAIT_S=1
+#
+# >&2 echo "DRX backend server - Waiting $WAIT_S seconds to allow nodes to start up"
+# sleep $WAIT_S
 >&2 echo "DRX backend server - starting up"
-# - server.port=8080
-# - config.rpc.host=0.0.0.0
-# - config.rpc.port=10006
-# - config.rpc.username=user1
-# - config.rpc.password=test
 
 cd /etc/exchange-backend/
-exec /usr/local/openjdk-8/bin/java -jar /etc/exchange-backend/clients-0.1.jar --server.port=8080 --config.rpc.host=192.168.3.1 --config.rpc.port=10001 --config.rpc.username=user1 --config.rpc.password=test
+exec /usr/local/openjdk-8/bin/java -jar /etc/exchange-backend/clients-0.1.jar --server.port=8080 --config.rpc.host=192.168.3.2 --config.rpc.port=10001 --config.rpc.username=user1 --config.rpc.password=test
