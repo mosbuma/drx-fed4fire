1983ef5c93a93b2bbd2ade6aeb51f9171e95e587
Marc Buma <mosbuma@bumos.nl>
2021-06-07 11:58:03 +0200

    remove oops

--- multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
+++ multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
@@ -9,3 +9,4 @@ set -e
 >&2 echo "DRX backend server - starting up"
 
 cd /etc/exchange-backend/
+exec /usr/local/openjdk-8/bin/java -jar /etc/exchange-backend/clients-0.1.jar --server.port=8080 --config.rpc.host=192.168.3.2 --config.rpc.port=10001 --config.rpc.username=user1 --config.rpc.password=test --config.rpc.retryDelay=5000
