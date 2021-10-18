82eda462897529bffb79b861b18f100f3340b72a
Marc Buma <mosbuma@bumos.nl>
2021-10-18 17:10:03 +0200

    check-in latest versions

--- multi-node/scripts/install-all-node-a.sh
+++ multi-node/scripts/install-all-node-a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget wget -O - -nv https://raw.githubusercontent.com/mosbuma/drx-fed4fire/master/multi-node/scripts/install-all-node.sh > ./install-all-node.sh
+wget -O - -nv https://raw.githubusercontent.com/mosbuma/drx-fed4fire/master/multi-node/scripts/install-all-node.sh > ./install-all-node.sh
 chmod +x ./install-all-node.sh
 source ./install-all-node.sh CopyrightDeltaA
