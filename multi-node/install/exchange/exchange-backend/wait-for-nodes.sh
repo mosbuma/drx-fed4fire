f95ee5d8b22aa731cd75ffa0da74a1423a2d3b0a
Miel Soeterbroek <miel.soeterbroek@gmail.com>
2021-10-20 11:06:45 +0200

    docker compose yml had a weird port mapping, executing the backend from wait_for_nodes.sh was still buggy too

--- multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
+++ multi-node/install/exchange/exchange-backend/wait-for-nodes.sh
@@ -7,6 +7,4 @@ set -e
 # >&2 echo "DRX backend server - Waiting $WAIT_S seconds to allow nodes to start up"
 # sleep $WAIT_S
 >&2 echo "DRX backend server - starting up"
-
-cd /drx-fed4fire/multi-node/install/exchange/exchange-backend
-exec /usr/local/openjdk-8/bin/java -jar /drx-fed4fire/multi-node/install/exchange/exchange-backend/clients-0.1.jar --server.port=8080 --config.rpc.host=192.168.3.2 --config.rpc.port=10006 --config.rpc.username=user1 --config.rpc.password=test --config.rpc.retryDelay=5000
+exec /usr/local/openjdk-8/bin/java -jar /etc/exchange-backend/clients-0.1.jar --server.port=8080 --config.rpc.host=192.168.3.2 --config.rpc.port=10006 --config.rpc.username=user1 --config.rpc.password=test --config.rpc.retryDelay=5000
